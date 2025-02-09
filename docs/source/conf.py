bd1c0ecc8b402e8a19277a2cb66b4da0e6622ebc
Mojeed Oyedeji <mojeed.oyedeji@gmail.com>
2025-02-09 22:52:42 +0300

    fix table of content

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,6 +5,17 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('.'))
+#content_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "content"))
+#sys.path.insert(0, content_path)
+
+#print(f"Sphinx is searching for content in: {content_path}")  # Debugging outpu
+#sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "content")))
+
+#content_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "content"))
+#sys.path.insert(0, content_path)
+
+#print(f"Sphinx is searching for content in: {content_path}")
+
 project = 'chetaa'
 copyright = '2024, Mo Oyedeji'
 author = 'Mo Oyedeji'
@@ -25,6 +36,11 @@ extensions = [
     "myst_parser"
 ]
 
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
+
 myst_enable_extensions = [
   'colon_fence',
   'attrs_block',
@@ -43,6 +59,13 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    "navigation_depth": 4,  # Keeps nested items visible
+    "collapse_navigation": False,  # Ensures sidebar stays expanded
+    "sticky_navigation": True,  # Keeps sidebar visible while scrolling
+    "includehidden": True,  # Ensures sub-items remain visible
+    "titles_only": False,  # Displays all headings, not just titles
+}
 #html_theme = "sphinx_book_theme"
 #html_theme = "alabaster"
 html_static_path = ['_static']
@@ -50,7 +73,6 @@ html_css_files = ['custom.css']
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
 
-plantuml = 'java -jar {}'.format(os.path.join(os.path.dirname(__file__), 'plantuml.jar'))
-
 sphinx_tabs_valid_builders = ['linkcheck']
 sphinx_tabs_disable_tab_closing = True
+
