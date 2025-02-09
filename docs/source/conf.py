5f748baaee2ab39e651452cc92cbe3124e2a3728
Mojeed Oyedeji <mojeed.oyedeji@gmail.com>
2025-02-09 19:46:13 +0300

    configure markdown

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,7 +42,8 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_book_theme"
 html_static_path = ['_static']
 html_css_files = ['custom.css']
 # -- Options for EPUB output
